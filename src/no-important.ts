e9897a0d2a0063c32d389e1c9e62e6c38b1b0dea
Lex Houf <lexhoufio@gmail.com>
2018-04-09 11:47:53 +0200

    Add support for no-important

--- src/no-important.ts
+++ src/no-important.ts
@@ -1,7 +1,7 @@
 import { ClassNameReplacer, replaceClassNames } from './replaceClassNames';
 import { createSerializer, serializer, SerializerOptions } from './serializer';
 
-const aphroditeSerializer = serializer(true);
+const aphroditeSerializer = serializer(false);
 const { test, print } = aphroditeSerializer;
 
 export {
